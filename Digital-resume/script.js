9c86d485a0824bb60dd58bc72608506d59f9afdd
Akeylah <112662745+Akeylah@users.noreply.github.com>
2025-07-07 22:07:20 +0000

    first commit

---
+++ Digital-resume/script.js
@@ -0,0 +1,7 @@
+document.getElementById('toggleMode').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  alert("This will be linked to a downloadable PDF soon!");
+});
